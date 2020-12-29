a7125735ce60028146d121e110a4904afa7e4996
unknown <farshinasri@gmail.com>
2020-12-29 09:21:45 +0330

    remove unNecessary dispatches

--- src/app.js
+++ src/app.js
@@ -7,27 +7,27 @@ import {addProfile,addFollower,setCurrentProfile} from './actions/profiles'
 
 const store=configureStore()
 
-//checking data:
-store.dispatch(addProfile('farshin','asri','c:'))
-store.dispatch(addProfile('reza','amiri','d:/'))
-store.dispatch(addProfile('hosein','javadi','d:/'))
-store.dispatch(addProfile('farshid','alipro','d:/'))
+// //checking data:
+// store.dispatch(addProfile('farshin','asri','c:'))
+// store.dispatch(addProfile('reza','amiri','d:/'))
+// store.dispatch(addProfile('hosein','javadi','d:/'))
+// store.dispatch(addProfile('farshid','alipro','d:/'))
 
-store.dispatch(setCurrentProfile(store.getState().profiles[0].id))
-console.log(store.getState())
-const idOfUsers=store.getState().profiles.filter(p=> p.id != store.getState().profiles[0].id )
+// store.dispatch(setCurrentProfile(store.getState().profiles[0].id))
+// console.log(store.getState())
+// const idOfUsers=store.getState().profiles.filter(p=> p.id != store.getState().profiles[0].id )
  
-idOfUsers.map(({id})=>{
+// idOfUsers.map(({id})=>{
 
-    store.dispatch(addFollower(id,store.getState().appConfig.currentId))
+//     store.dispatch(addFollower(id,store.getState().appConfig.currentId))
 
-})
-//console.log(store.getState().appConfig.currentId)
-////
+// })
+// //console.log(store.getState().appConfig.currentId)
+// ////
 
-store.subscribe(()=>{
-    console.log(store.getState())
-})
+// store.subscribe(()=>{
+//     console.log(store.getState())
+// })
 
 const jsx=(
     <Provider store={store}>
